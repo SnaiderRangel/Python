49ad49f6647f8e4c26b21b056509788f56c08cbf
Snaider Rangel <xtrack1921@gmail.com>
2022-11-25 11:07:54 -0500

    New file

---
+++ main.js
@@ -0,0 +1,6 @@
+// condicionales
+
+if(12 < 13){
+    console.log('Hi my name is: Julian');
+    console.log('Comand not a funt: Error 1213');
+}
---
+++ main.py
@@ -0,0 +1,14 @@
+#Tipos de datos
+
+# print('Hi, world')
+# print(12 + 1)
+
+"""
+Otro tipo de comentario 
+
+"""
+# Condicionales
+
+# if 12 < 13:
+#     print('Hi, my name is: Antoni')
+
